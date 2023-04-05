d90d244484fa98b2e3affa9e1c11fa9e84fbddf3
Drstocktor <talibanvicke@live.se>
2023-04-05 06:36:28 +0200

    Initial commit

---
+++ src/main/java/com/example/examinationslabb/ExaminationslabbApplication.java
@@ -0,0 +1,13 @@
+package com.example.examinationslabb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationslabbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminationslabbApplication.class, args);
+    }
+
+}
